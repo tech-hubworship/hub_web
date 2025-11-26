6be0856a8fb2e6a696f3f37877fa3adf595126ce
KOH JOOWON <joowonkoh@naver.com>
2025-11-26 23:55:41 +0900

    feat: 이벤트 로직 변경

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -4,13 +4,14 @@ export const Wrapper = styled.header<{ opacity: number; isMenuOpen?: boolean }>`
   width: 100%;
   min-height: 80px;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   position: fixed;
   z-index: 100;
   top: 0;
-  padding: 0 20px;
-
+  left: 0;
+  right: 0;
+  padding: 0 40px;
 
   background-color: ${({ opacity, isMenuOpen }) => 
     isMenuOpen ? 'rgba(255, 255, 255, 1)' : `rgba(255, 255, 255, ${opacity})`};
@@ -18,24 +19,18 @@ export const Wrapper = styled.header<{ opacity: number; isMenuOpen?: boolean }>`
   transition: background-color 0.2s ease, backdrop-filter 0.2s ease;
 
   @media (min-width: 58.75rem) {
-    width: 600px;
-    left: 50%;
-    transform: translateX(-50%);
+    height: 80px;
     border-bottom: 1px solid rgba(0, 0, 0, 0.05);
-    justify-content: space-between;
   }
 
   @media (max-width: 58.75rem) and (min-width: 48rem) {
     height: 60px;
-    padding: 0;
-    justify-content: space-between;
+    padding: 0 20px;
   }
 
   @media (max-width: 47.9375rem) {
     height: 60px;
     min-height: 60px;
-    padding: 0;
-    justify-content: space-between;
+    padding: 0 16px;
   }
-
 `;
