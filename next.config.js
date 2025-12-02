2742c3bb1934cbffd31a0791e8a46daa73894554
KOH JOOWON <joowonkoh@naver.com>
2025-12-02 18:33:46 +0900

    feat : favicon.ico edge request 최적화

--- next.config.js
+++ next.config.js
@@ -20,6 +20,28 @@ const nextConfig = {
     });
     return config;
   },
+  async headers() {
+    return [
+      {
+        source: '/favicon.ico',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/favicon-:size.png',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
   async redirects() {
     return [
       {
