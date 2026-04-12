4f35cea3d747a88f4769a860b1ae3f732828ab00
YeongKyuKang <47838023+YeongKyuKang@users.noreply.github.com>
2026-04-12 11:44:09 +0900

    fix::banner error

--- src/views/MainPage/components/Banner/style.ts
+++ src/views/MainPage/components/Banner/style.ts
@@ -2,7 +2,7 @@ import styled from "@emotion/styled";
 
 export const Container = styled.section`
   width: 100%;
-  height: calc((480 / 360) * 100vw);
+  aspect-ratio: 1080 / 480;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -11,17 +11,11 @@ export const Container = styled.section`
   background-image: url('/images/Welcome.png');
   background-position: center;
   background-repeat: no-repeat;
-  background-size: cover;
+  background-size: contain;
   position: relative;
 
-  @media (min-width: 58.75rem) {
-    height: 480px;
-    width: 100%;
-    background-size: cover;
-  }
-
   @media (max-width: 768px) {
-    height: calc((480 / 360) * 100vw);
-    min-height: 300px;
+    aspect-ratio: 1080 / 480;
+    min-height: 180px;
   }
 `;
